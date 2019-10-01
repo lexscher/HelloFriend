fdb3ff8f6c7da33acbf0ce5175dc1adf439aebef
Alexander Schelchere <ascellie@gmail.com>
2019-10-01 06:38:26 -0400

    animals controller - index, show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def index
+    animals = Animal.all
+    render json: animals, status: :ok
+  end
+
+  def show
+    animal = Animal.find(params[:id])
+    render json: { stats: animal, introduction: animal.intro }, status: :ok
+  end
 end
