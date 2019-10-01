2b930b8da7f4698c9a83a5289fea549e2e5e7f28
Alexander Schelchere <ascellie@gmail.com>
2019-10-01 06:51:36 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+spot = Animal.create(name: 'Spot', species: 'Dog')
+garfield = Animal.create(name: 'Garfield', species: 'Cat')
+krypto = Animal.create(name: 'Krypto', species: 'Super Dog')
+felix = Animal.create(name: 'Felix', species: 'Cat')
+scooby = Animal.create(name: 'Scooby Doo', species: 'Dog')
+grodd = Animal.create(name: 'Grodd', species: 'Gorilla')
+gary = Animal.create(name: 'Gary', species: 'Snail')
+hedwig = Animal.create(name: 'Hedwig', species: 'Owl')
+tom = Animal.create(name: 'Tom', species: 'Cat')
+jerry = Animal.create(name: 'Jerry', species: 'Mouse')
+buggs = Animal.create(name: 'Buggs', species: 'Bunny')
+mickey = Animal.create(name: 'Mickey', species: 'Mouse')
+minnie = Animal.create(name: 'Minnie', species: 'Mouse')
+donald = Animal.create(name: 'Donald', species: 'Duck')
