cfdee3f56ceb93fabef52722acc3ee00dcb37481
Alexander Schelchere <ascellie@gmail.com>
2019-10-01 06:39:34 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :animals
+  resources :animals, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
